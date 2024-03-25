b3292827b14e388b2e19bc056403e33b4f1eef2c
Yasas-Sri <ezioelijahhanaang@gmail.com>
2024-03-25 12:58:42 +0530

    Add changes-homepage

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import 'swiper/css';
+
+// bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+// fonts and icons
+import '././assets/css/icofont.min.css';
+import '././assets/css/animate.css';
+import '././assets/css/style.min.css';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<App/>,
+    children:[
+      {path:"/",
+          element:<Home/>
+    }
+    ]
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
